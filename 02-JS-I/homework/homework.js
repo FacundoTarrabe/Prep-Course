9f760486e48a01df490753f7755248854e5354f7
FacundoTarrabe <tarragelp@gmail.com>
2022-06-17 14:03:12 -0300

    8

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = Hola;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 5 + 3;
+const nuevoNum = 5+3;
 
 // Crea una variable booleana:
 const nuevoBool = null;
